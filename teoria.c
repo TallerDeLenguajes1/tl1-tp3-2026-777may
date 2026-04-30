8124bb8f2a8bdd59ae8085f5e8c047d2445059be
Mayra <mayri.luli@gmail.com>
2026-04-30 19:47:45 -0300

    apuntes

--- teoria.c
+++ teoria.c
@@ -6,13 +6,15 @@ int main()
 {
     
     int largoNombre;
+    char buff[50];
     for (int i = 0; i < 5; i++)
     {
         printf("nombre estudiante %d", i);
         scanf("%s", buff);
         largoNombre = strlen(buff);
-        nombreAlumno[i] = malloc(sizeof(char)*largoNombre);
+        nombreAlumno[i] = (char*)malloc(sizeof(char)*largoNombre);
         strcpy(nombreAlumno[i], buff);
+        free(nombeAlumno);
     }
     
     return 0;
