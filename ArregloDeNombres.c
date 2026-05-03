852a68e7dd9b5a68efc924c53ac37e6167f2529d
Mayra <mayri.luli@gmail.com>
2026-05-03 18:00:28 -0300

    cambios en arreglo

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void MostrarPersonas(){
-    printf("El alumno %d se llama: %s\n", i, );
 
-}
+#define CantNombres 5
+
+void MostrarPersonas(char* vector[]);
+
 int main()
 {
-    printf("Ingrese el nombre del alumno %d: \n", i);
-
+    char* V[CantNombres];
+    char Buff[100];
+    printf("Bienvenido, por favor ingrese los nombres de las personas.");
+    for (int i = 0; i < CantNombres; i++)
+    {
+        printf("\nIngrese el %d° nombre: \n", i+1);
+        gets(Buff);
+        V[i] = (char*) malloc(sizeof(char)*strlen(Buff) +1);
+        strcpy(V[i], Buff);
+    }
+    MostrarPersonas(V);
+    
+    
     return 0;
+}
+void MostrarPersonas(char* vector[]){
+    for (int i = 0; i < CantNombres; i++)
+    {
+        printf("El %d° nombre es: %s\n", i+1, vector[i]);
+    }
+
 }
